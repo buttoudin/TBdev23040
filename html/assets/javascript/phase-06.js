53364904d1e1bdcb7494215c72c0f2b602acc403
clementafpa <clementbut001@gmail.com>
2023-07-03 13:45:28 +0200

    exercice06

---
+++ html/assets/javascript/phase-06.js
@@ -0,0 +1,13 @@
+var a = '100';
+var b = 100;
+var c = 1.00;
+var d = true;
+
+window.alert("Ceci est une chaine de caractères:" + a);
+
+b = b--;
+window.alert(b);
+c = Number(c) + Number(a);
+window.alert(c);
+
+d = false;
