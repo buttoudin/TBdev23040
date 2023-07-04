9b28dca01e0383253c05eefbf8785074dfd121bc
clementafpa <clementbut001@gmail.com>
2023-07-04 15:37:37 +0200

    phase8 exercice2

--- html/assets/javascript/phase-08.js
+++ html/assets/javascript/phase-08.js
@@ -1,15 +1,5 @@
-var prenom = "";
-var a = 0;
-var tableau = [];
-
-prenom = window.prompt("entrez un prénom");
-console.log(tableau[0] = [prenom]);
-
-while (prenom != '') {
-    a = a + 1;
-    prenom = window.prompt("entrez un autre prénom");
-    if (prenom != "") {
-        console.log(tableau[a] = [prenom]);
-    }
+var n=window.prompt("choisissez un nombre");
+while (n!=0) {
+    window.alert(n-1);
+    n=n-1;
 }
-console.log(a + " prénoms d'écris");
