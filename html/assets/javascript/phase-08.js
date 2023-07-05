26d2bd3ed956eba2c8f925c11d826377ac96db47
clementafpa <clementbut001@gmail.com>
2023-07-05 15:31:58 +0200

    phase08-exercice3

--- html/assets/javascript/phase-08.js
+++ html/assets/javascript/phase-08.js
@@ -1,5 +1,21 @@
-var n=window.prompt("choisissez un nombre");
-while (n!=0) {
-    window.alert(n-1);
-    n=n-1;
+var n = 0;
+var a = "";
+var tableau = [];
+var b = 0;
+var c = 0;
+var somme = 0;
+var moy = 0;
+while (a == "") {
+    n = window.prompt("choisissez un nombre entier");
+    tableau[b] = [n];
+    b = b + 1;
+    a = window.prompt("ne rien écrire pour continuer et stop pour arréter");
 }
+while (c != b) {
+    somme = Number(tableau[c]) + Number(somme);
+    c = c + 1;
+
+}
+moy = somme / (b);
+window.alert("la somme est de " + somme);
+window.alert("la moyenne est de " + moy);
