3a8a1413b39d2e8502b12017914feb762477a24d
clementafpa <clementbut001@gmail.com>
2023-07-12 09:08:36 +0200

    phase 13 exercice

---
+++ javascript/phase-13/assets/javascript/phase-13.js
@@ -0,0 +1,5 @@
+alert("<h1>Coucou !<h1>");
+//les balise html ne sont pas prisent en compte par la fonction alert()
+console.log("Ah que Coucou !");
+document.write("Coucou beuh !<br>");
+document.write("mon premier script");
