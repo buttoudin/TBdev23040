581d42b22cc81a35575cc943071425ce4a4698c1
clementafpa <clementbut001@gmail.com>
2023-07-21 10:39:35 +0200

    evaluation exercice 1 correction

--- javascript/evaluation/asset/javascript/exercice1.js
+++ javascript/evaluation/asset/javascript/exercice1.js
@@ -10,15 +10,15 @@ while (age < 100) {
 }
 
 for (let i = 0; i < mem.length; i++) {
-if (mem[i]<20) {
-    jeune = +jeune+1;
-}
-else if (mem[i]>40) {
-    vieux = +vieux + 1;
-}
-else{
-    moyen = +moyen + 1;
-}
+    if (mem[i] < 20) {
+        jeune = +jeune + 1;
+    }
+    else if (mem[i] > 40) {
+        vieux = +vieux + 1;
+    }
+    else if (mem[i] >= 20 && mem[i] <= 40) {
+        moyen = +moyen + 1;
+    }
 
 }
-window.alert('il y a ' + jeune + 'jeune. Il y a ' + moyen + "personne d'âge moyen. Il y a " + vieux + "personne âgée.")
+window.alert('Il y a ' + jeune + 'jeune. Il y a ' + moyen + " personne d'âge moyen. Il y a " + vieux + " personne âgée.")
